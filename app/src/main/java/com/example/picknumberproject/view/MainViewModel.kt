2d72f24e06ce9e168d2f07773c08b1c41e5bdd13
oh in seong <superohinsung@naver.com>
2023-02-18 21:31:19 +0900

    필요없는 import값 삭제

--- app/src/main/java/com/example/picknumberproject/view/MainViewModel.kt
+++ app/src/main/java/com/example/picknumberproject/view/MainViewModel.kt
@@ -1,14 +1,6 @@
 package com.example.picknumberproject.view
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.picknumberproject.data.repository.BankRepository
-import com.example.picknumberproject.view.map.MapUiState
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 class MainViewModel() : ViewModel() {
     // TODO : 뭐가 들어갈지 생각해봅시다...
