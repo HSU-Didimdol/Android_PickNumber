f9948b0369dca856ddb4a8a3df992b3a60177894
oh in seong <superohinsung@naver.com>
2023-03-10 17:39:27 +0900

    viewbinding setting

--- app/src/main/java/com/example/picknumberproject/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/picknumberproject/view/login/LoginActivity.kt
@@ -1,10 +1,18 @@
 package com.example.picknumberproject.view.login
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
 import com.example.picknumberproject.R
+import com.example.picknumberproject.databinding.ActivityLoginBinding
+import com.example.picknumberproject.view.common.ViewBindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoginActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class LoginActivity : ViewBindingActivity<ActivityLoginBinding>() {
+
+    override val bindingInflater: (LayoutInflater) -> ActivityLoginBinding
+        get() = ActivityLoginBinding::inflate
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
--- app/src/main/java/com/example/picknumberproject/view/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/picknumberproject/view/signup/SignUpActivity.kt
@@ -1,10 +1,18 @@
 package com.example.picknumberproject.view.signup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.picknumberproject.R
+import com.example.picknumberproject.databinding.ActivitySignUpBinding
+import com.example.picknumberproject.view.common.ViewBindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class SignUpActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class SignUpActivity : ViewBindingActivity<ActivitySignUpBinding>() {
+
+    override val bindingInflater: (LayoutInflater) -> ActivitySignUpBinding
+        get() = ActivitySignUpBinding::inflate
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
