b375f88b0d766233e670708f008ec5fc4eebc9db
oh in seong <superohinsung@naver.com>
2023-06-09 02:49:38 +0900

    기본적인것들 변경

--- app/src/main/java/com/example/picknumberproject/data/url/Key.kt
+++ app/src/main/java/com/example/picknumberproject/data/url/Key.kt
@@ -1,8 +1,8 @@
 package com.example.picknumberproject.data.url
 
 object Key {
-    const val clientID = "saz36oia18"
-    const val clientSecret = "tAHQt1wXWFWasAPaxsZ8tDrtKT9zI7sspCsw388P"
+    const val clientID = "f1qm5gy16z"
+    const val clientSecret = "PJ5FKlGtMjQ4lwZLVSPul185mSDiKprEaWjCYtpI"
     const val x_access_token = "F077C3E655E66E9F1F86F5CA36C4FEAF"
     const val reservation_token = "F077C3E655E656703D347971A9803B0B"
 }
